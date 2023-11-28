6f27fd8df55aae103b83245e2def3c6cf048dbd0
lucien-wrq <luluwerq@gmail.com>
2023-11-28 13:51:29 +0100

    This commit adds two new tables, "Events" and "Agenda", to the database.
     (generated by blackbox.ai )

--- commande.sql
+++ commande.sql
@@ -15,3 +15,12 @@ CREATE TABLE "Groups" (
 	"Groups_name"	TEXT NOT NULL
 )
 
+CREATE TABLE "Events" (
+	"Id_Events"	INTEGER NOT NULL UNIQUE,
+	"Title"	TEXT NOT NULL
+)
+
+CREATE TABLE "Agenda" (
+	"Id_Agenda"	INTEGER NOT NULL UNIQUE,
+	"Agenda_Name"	TEXT NOT NULL
+)
