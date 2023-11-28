bbc33d0ebc31caf03fdbe5cad9b784767d4ef7d4
giantprolu <nathnathchav@gmail.com>
2023-11-28 15:21:28 +0100

    echo partout "y'en pas beaucoup"

--- Back/inserer.php
+++ Back/inserer.php
@@ -4,33 +4,37 @@ $Id_Users = $_POST['Id'];
 $name = $_POST['Name'];
 $vorname = $_POST['Vorname'];
 $email = $_POST['Email'];
-
+echo $email;
 // Ouvrir la connexion à la base de données SQLite
 try {
-    $db = new PDO('sqlite:BDD.db');
+    $db = new PDO('C:\wamp64\www\agenda\Back\BDD.db');
+    echo $email;
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
 
 // Préparer la requête SQL
 $sql = "INSERT INTO votre_table (Id_Users, name, vorname, email) VALUES (:Id_Users, :name, :vorname, :email)";
-
+echo $email;
 // Préparer la requête SQL avec PDO
 $stmt = $db->prepare($sql);
-
+echo $email;
 // Binder les paramètres
 $stmt->bindParam(':Id_Users', $Id_Users, PDO::PARAM_INT);
 $stmt->bindParam(':name', $name, PDO::PARAM_STR);
 $stmt->bindParam(':vorname', $vorname, PDO::PARAM_STR);
 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-
+echo $email;
 // Exécuter la requête
 if ($stmt->execute()) {
     echo "Enregistrement réussi";
+    echo $email;
 } else {
     echo "Erreur : " . print_r($stmt->errorInfo(), true);
+    echo $email;
 }
 
 // Fermer la connexion
 $db = null;
+echo $email;
 ?>
