9c31004d4ede638396d7006e9b1d21734fc94ee9
giantprolu <nathnathchav@gmail.com>
2023-11-28 17:27:01 +0100

    crea event

--- Back/creation_event.php
+++ Back/creation_event.php
@@ -1,33 +1,37 @@
 <?php
 // Récupérer les données du formulaire HTML
-$titre = $_POST['title'];
-$date = $_POST['date'];
-$heureDebut = $_POST['start_date'];
-$heureFin = $_POST['end_date'];
-// Ajoutez d'autres champs selon les besoins (par exemple, lieu, description, etc.)
+$title = isset($_POST['title']) ? $_POST['title'] : '';
+$date = isset($_POST['date']) ? $_POST['date'] : '';
+$start_date = isset($_POST['start_date']) ? $_POST['start_date'] : '';
+$end_date = isset($_POST['end_date']) ? $_POST['end_date'] : '';
 
 try {
     $db = new PDO('sqlite:BDD.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exception
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
 
 // Préparer la requête SQL
-$sql = "INSERT INTO evenements (title, date, start_date, end_date) VALUES ('$titre', '$date', '$heureDebut', '$heureFin')";
+$sql = "INSERT INTO Events (title, date) VALUES (:title, :date,)";
+$sql = "INSERT INTO edit (start_date, end_date) VALUES (:start_date, :end_date)";
+// Préparer la requête SQL avec PDO
+$stmt = $db->prepare($sql);
 
-
-$stmt->bindParam(':title', $titre, PDO::PARAM_STR);
-$stmt->bindParam(':date', $date, PDO::PARAM_INT);
-$stmt->bindParam(':start_date', $heureDebut, PDO::PARAM_INT);
-$stmt->bindParam(':end_date', $heureFin, PDO::PARAM_INT);
+// Binder les paramètres
+$stmt->bindParam(':title', $title, PDO::PARAM_STR);
+$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+$stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+$stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
 
 // Exécuter la requête
-if ($conn->query($sql) === TRUE) {
-    echo "Événement créé avec succès";
-} else {
-    echo "Erreur : " . $sql . "<br>" . $conn->error;
+try {
+    $stmt->execute();
+    echo "Enregistrement réussi";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
 }
 
 // Fermer la connexion
-$conn->close();
+$db = null;
 ?>
