f7dadfb7b4f562e6d8a2b32643c25792e38fc203
giantprolu <nathnathchav@gmail.com>
2023-11-28 15:02:31 +0100

    inserer changement

--- Back/inserer.php
+++ Back/inserer.php
@@ -41,3 +41,40 @@ try {
     echo "Erreur: " . $e->getMessage();
 }
 ?>
+
+<?php
+// Récupérer les données du formulaire HTML
+$id = $_POST['id'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+
+// Ouvrir la connexion à la base de données SQLite
+try {
+    $db = new PDO('sqlite:BDD.db');
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+// Préparer la requête SQL
+$sql = "INSERT INTO votre_table (id, prenom, nom, email) VALUES (:id, :prenom, :nom, :email)";
+
+// Préparer la requête SQL avec PDO
+$stmt = $db->prepare($sql);
+
+// Binder les paramètres
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt->bindParam(':prenom', $firstname, PDO::PARAM_STR);
+$stmt->bindParam(':nom', $lastname, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+// Exécuter la requête
+if ($stmt->execute()) {
+    echo "Enregistrement réussi";
+} else {
+    echo "Erreur : " . print_r($stmt->errorInfo(), true);
+}
+
+// Fermer la connexion
+$db = null;
+?>
