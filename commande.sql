f32cb0cb68c095620adeed1912fab71d1d68f36b
lucien-wrq <luluwerq@gmail.com>
2023-11-28 13:48:35 +0100

    table

---
+++ commande.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "Roles" (
+	"id_role"	INTEGER NOT NULL UNIQUE,
+	", role_nam"	TEXT
+)
+
+CREATE TABLE "Users" (
+	"ID_Users"	INTEGER NOT NULL UNIQUE,
+	"Name"	TEXT NOT NULL,
+	"vorname"	TEXT NOT NULL,
+	"email"	TEXT NOT NULL
+)
+
