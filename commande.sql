725ade0d7aa02f73d1964e153dcdd5b7ade90773
lucien-wrq <luluwerq@gmail.com>
2023-11-28 14:03:02 +0100

    This commit adds 7 new tables to the database: Users, Events, Groups, Roles, Possede, Appartenir, and edit.
    The Users table stores information about users, including their name, first name, and email address.
    
    The Events table stores information about events, including their title and description.
    
    The Groups table stores information about groups, including their name.
    
    The Roles table stores information about roles, including their name.
    
    The Possede table stores information about which users have which roles.
    
    The Appartenir table stores information about which users belong to which groups.
    
    The edit table stores information about which users can edit which events and groups, and the start and end dates of those events. (generated by blackbox.ai )

--- commande.sql
+++ commande.sql
@@ -1,26 +1,57 @@
-CREATE TABLE "Roles" (
-	"id_role"	INTEGER NOT NULL UNIQUE,
-	"role_nam"	TEXT
-)
+CREATE TABLE Users(
+   Id_Users INT,
+   name VARCHAR(50) NOT NULL,
+   vorname VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Users),
+   UNIQUE(email)
+);
 
-CREATE TABLE "Users" (
-	"ID_Users"	INTEGER NOT NULL UNIQUE,
-	"Name"	TEXT NOT NULL,
-	"vorname"	TEXT NOT NULL,
-	"email"	TEXT NOT NULL
-)
+CREATE TABLE Events(
+   Id_Events INT,
+   title VARCHAR(50) NOT NULL,
+   description VARCHAR(500),
+   PRIMARY KEY(Id_Events)
+);
 
-CREATE TABLE "Groups" (
-	"Id_Groups"	INTEGER NOT NULL UNIQUE,
-	"Groups_name"	TEXT NOT NULL
-)
+CREATE TABLE Groups(
+   Id_group INT,
+   group_name VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_group),
+   UNIQUE(group_name)
+);
 
-CREATE TABLE "Events" (
-	"Id_Events"	INTEGER NOT NULL UNIQUE,
-	"Title"	TEXT NOT NULL
-)
+CREATE TABLE Roles(
+   Id_role INT,
+   role_name VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_role),
+   UNIQUE(role_name)
+);
 
-CREATE TABLE "Agenda" (
-	"Id_Agenda"	INTEGER NOT NULL UNIQUE,
-	"Agenda_Name"	TEXT NOT NULL
-)
+CREATE TABLE Possede(
+   Id_Users INT,
+   Id_role INT,
+   PRIMARY KEY(Id_Users, Id_role),
+   FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+   FOREIGN KEY(Id_role) REFERENCES Roles(Id_role)
+);
+
+CREATE TABLE Appartenir(
+   Id_Users INT,
+   Id_group INT,
+   PRIMARY KEY(Id_Users, Id_group),
+   FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+   FOREIGN KEY(Id_group) REFERENCES Groups(Id_group)
+);
+
+CREATE TABLE edit(
+   Id_Users INT,
+   Id_Events INT,
+   Id_group INT,
+   start_date DATETIME NOT NULL,
+   end_date DATETIME NOT NULL,
+   PRIMARY KEY(Id_Users, Id_Events, Id_group),
+   FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+   FOREIGN KEY(Id_Events) REFERENCES Events(Id_Events),
+   FOREIGN KEY(Id_group) REFERENCES Groups(Id_group)
+);
