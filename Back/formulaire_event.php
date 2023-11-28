e5fe1bc5cd135ac81b42d77dcfe12bdbc7560bbc
giantprolu <nathnathchav@gmail.com>
2023-11-28 16:53:28 +0100

    test crétaion event

--- Back/formulaire_event.php
+++ Back/formulaire_event.php
@@ -16,10 +16,12 @@
         <label for="date">Sélectionnez une date :</label>
         <input type="date" name="date" required value="<?php $date?>">
         </p>
-        <p>
-            <label> Email </label>
-            <input type="texte" name="Email" value="<?php $email?>">
-        </p>
+        <label> Plage Horaire </label>
+        <label for="heure_debut">Heure de début :</label>
+        <input type="time" id="heure_debut" name="heure_debut" required>
+
+        <label for="heure_fin">Heure de fin :</label>
+        <input type="time" id="heure_fin" name="heure_fin" required>
         <p> 
             <input type="submit" value="Valider" name="Valider">
         </p>
