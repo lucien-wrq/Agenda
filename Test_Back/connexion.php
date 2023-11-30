3bac6c59f52ab48d745238ba6e0f3f6ecb300fb0
lucien-wrq <luluwerq@gmail.com>
2023-11-30 09:45:28 +0100

    The commit changes the database credentials and adds a query to select all users from the database. (generated by blackbox.ai )

--- Test_Back/connexion.php
+++ Test_Back/connexion.php
@@ -1,11 +1,17 @@
 <?php
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$database = "your_database";
+$username = "root";
+$password = "";
+$database = "agenda";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
+
+$sql = "SELECT * From users";
+
+// Préparer la requête SQL avec PDO
+$stmt = $db->prepare($sql);
+$stmt->execute();
 
 $conn->close();
 ?>
