58eafe6e62ede40caf2be2be6b6689a09a783f10
giantprolu <nathnathchav@gmail.com>
2023-11-30 09:51:51 +0100

    test connexion.php

--- Test_Back/connexion.php
+++ Test_Back/connexion.php
@@ -7,8 +7,26 @@ $database = "agenda";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-$sql = "SELECT * From users";
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Users";
 $result = $conn->query($sql);
 
+if ($result) {
+
+    if ($result->num_rows > 0) {
+        // Output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id_user"] . " - Nom: " . $row["lastname"] . " - Prenom: " . $row["firstname"] .  " - email: " . $row["email"];
+            // You can customize the output based on your table columns
+        }
+    } else {
+        echo "No records found";
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 $conn->close();
 ?>
