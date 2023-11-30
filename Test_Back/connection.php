2688c874ad3263b54bf547cf8f75d88f30410db3
giantprolu <nathnathchav@gmail.com>
2023-11-30 09:35:38 +0100

    MAJ code connexion.php

--- Test_Back/connection.php
+++ Test_Back/connection.php
@@ -1,3 +1,11 @@
 <?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+$conn->close();
 ?>
